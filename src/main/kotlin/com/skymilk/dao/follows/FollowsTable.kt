19c0d1caf3926d13a239d650103f2bf5aaa889e0
beh0907 <qkrdlsguq@naver.com>
2025-01-19 16:46:21 +0900

    version 0.17
    - 팔로우 테이블 참조 키 설정

--- src/main/kotlin/com/skymilk/dao/follows/FollowsTable.kt
+++ src/main/kotlin/com/skymilk/dao/follows/FollowsTable.kt
@@ -1,12 +1,14 @@
 package com.skymilk.dao.follows
 
+import com.skymilk.dao.user.UserTable
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.kotlin.datetime.CurrentDateTime
 import org.jetbrains.exposed.sql.kotlin.datetime.datetime
 
 object FollowsTable: Table(name = "follows") {
-    val followerId = long(name = "follower_id").autoIncrement()
-    val followingId = long(name = "following_id").autoIncrement()
+    val followerId = long(name = "follower_id").autoIncrement().references(ref = UserTable.id, onDelete = ReferenceOption.CASCADE)
+    val followingId = long(name = "following_id").autoIncrement().references(ref = UserTable.id, onDelete = ReferenceOption.CASCADE)
     val followDate = datetime(name = "follow_date").defaultExpression(defaultValue = CurrentDateTime)
 }
 
